36570bf3a311cebee6bc78f7b8f31e0bd2df2a48
Giulia <giuliatullia93@gmail.com>
2021-09-12 02:13:47 +0200

    base header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,24 @@
+<header>
+
+<!-- logo navbar -->
+    <div class="logo">
+        <img src="{{asset('images/dc-logo.png')}}" alt="">
+    </div>
+
+    <nav>
+        <ul>
+            <li><a href="{{route('')}}">Characters</a></li>
+            <li><a href="{{route('')}}">Comics</a></li>
+            <li><a href="{{route('')}}">Movies</a></li>
+            <li><a href="{{route('')}}">Tv</a></li>
+            <li><a href="{{route('')}}">Games</a></li>
+            <li><a href="{{route('')}}">Collectibles</a></li>
+            <li><a href="{{route('')}}">Videos</a></li>
+            <li><a href="{{route('')}}">Fans</a></li>
+            <li><a href="{{route('')}}">News</a></li>
+            <li><a href="{{route('')}}">Shop</a></li>
+        </ul>
+    </nav>
+
+
+</header>
